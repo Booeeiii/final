fdc86ada1d09576647ca7dbf2f2396ec2ea2f5c3
Booeeiii <73687103+Booeeiii@users.noreply.github.com>
2020-11-27 13:48:03 +0700

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ITF Lab</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'ihost.it.kmitl.ac.th', 'it63070218_it63070218', 'PPVvie84', 'it63070218_Yanisa', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$A = $_POST['A'];
+$B = $_POST['B'];
+
+
+$sql = "INSERT INTO guestbook (A , B) VALUES ('A', 'B')";
+
+
+if (mysqli_query($conn, $sql)) {
+     echo '<div class="container mt-4">
+            <h3>successfully.</h3>
+            <a role="button" class="btn btn-outline-secondary" href="show (2).php"Next</a>
+         </div>';
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+
+?>
+</body>
+</htmll>
